d0bdb71e95568f59720435d9b8a339fda5a24249
Volodymyr Shostak <3d.dailylviv@gmail.com>
2022-05-22 14:11:28 +0300

    another changes

--- src/If/If1.java
+++ src/If/If1.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 
 public class If1 {
     public static void main(String[] args) {
+
+String s1 = "hello";
+String s2 = "hello";
+        System.out.println(s1==s2);
+
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter an integer: ");
+         double sa = 5/2;
+
+
+
         int a = sc.nextInt();
 
         if (a >= 0) {
@@ -13,6 +23,6 @@ public class If1 {
         } else {
             System.out.println("The number: " + a);
         }
-
+        System.out.println(sa);
     }
 }
