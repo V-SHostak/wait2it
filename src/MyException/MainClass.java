9ee182fad2a04e0e46249a00a7d4a5e758833c52
Volodymyr Shostak <3d.dailylviv@gmail.com>
2022-06-27 01:47:47 +0300

    Corrected package MyException

--- src/MyException/MainClass.java
+++ src/MyException/MainClass.java
@@ -7,50 +7,26 @@ public class MainClass {
 
         try {
             System.out.println(methods.addition(5, 0));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        } catch (ArithmeticException e) {
-            System.out.println(e);
-        } catch (IllegalAccessException e) {
-            System.out.println(e);
-        } catch (MyException e) {
+        } catch (IllegalArgumentException | IllegalAccessException | ArithmeticException | MyException e) {
             System.out.println(e);
         }
 
 
         try {
             System.out.println(methods.multiplication(0, 0));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        } catch (ArithmeticException e) {
-            System.out.println(e);
-        } catch (IllegalAccessException e) {
-            System.out.println(e);
-        } catch (MyException e) {
+        }  catch (IllegalArgumentException | IllegalAccessException | ArithmeticException | MyException e) {
             System.out.println(e);
         }
 
         try {
             System.out.println(methods.division(-3, -2));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        } catch (ArithmeticException e) {
-            System.out.println(e);
-        } catch (IllegalAccessException e) {
-            System.out.println(e);
-        } catch (MyException e) {
+        }  catch (IllegalArgumentException | IllegalAccessException | ArithmeticException | MyException e)  {
             System.out.println(e);
         }
 
         try {
             System.out.println(methods.subtraction(3, 9));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        } catch (ArithmeticException e) {
-            System.out.println(e);
-        } catch (IllegalAccessException e) {
-            System.out.println(e);
-        } catch (MyException e) {
+        }  catch (IllegalArgumentException | IllegalAccessException | ArithmeticException | MyException e)  {
             System.out.println(e);
         }
 
