1a9f53b3415863fe3707378a7035942d34a0ef70
Volodymyr Shostak <3d.dailylviv@gmail.com>
2022-06-29 01:09:12 +0300

    Add package MyListlement

---
+++ src/MyListElement/MyListElement.java
@@ -0,0 +1,38 @@
+package MyListElement;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MyListElement {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(55);
+        numbers.add(42);
+        numbers.add(32);
+
+        ListIterator<Integer> listIterator = numbers.listIterator();
+
+        while (listIterator.hasNext()) {
+            int x = listIterator.next() * 5;
+            System.out.println(x);
+        }
+
+
+        System.out.println("---------------------------------------------");
+
+        Iterator<Integer> listIterator2 = numbers.iterator();
+        while (listIterator2.hasNext()) {
+            System.out.println(listIterator2.next());
+        }
+
+        System.out.println("---------------------------------------------");
+
+        while (listIterator.hasPrevious()) {
+            System.out.println(listIterator.previous());
+        }
+    }
+}
